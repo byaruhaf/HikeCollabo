d83fb171de35a70052a8014decf7c7825ae89e6e
Franklin Byaruhanga <byaruhaf@protonmail.com>
2023-06-11 04:59:10 +0300

    Initial Commit

---
+++ HikeCollabo/HikeCollaboApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeCollaboApp.swift
+//  HikeCollabo
+//
+//  Created by Franklin Byaruhanga on 11/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeCollaboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
